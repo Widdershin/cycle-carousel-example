a15fd9a98efec32a7552cc650c36f862705c866e
Raquel Moss <raquelxmoss@gmail.com>
2016-01-07 17:22:28 +1300

    convert to flexbox

--- src/app.js
+++ src/app.js
@@ -1,39 +1,39 @@
 import {Observable} from 'rx';
 import {div, img, button} from 'cycle-snabbdom';
 
-const catPictures = [
-  'http://25.media.tumblr.com/Jjkybd3nScerz2jsjvrqDPix_400.jpg',
-  'http://25.media.tumblr.com/tumblr_lny14qJ7Wu1qdth8zo1_1280.jpg',
-  'http://25.media.tumblr.com/tumblr_mbo4u98wvQ1qhwmnpo1_1280.jpg',
-  'http://30.media.tumblr.com/tumblr_m26a45YwZA1r6t7tvo1_500.gif',
-  'http://25.media.tumblr.com/tumblr_m2x23lyfit1qzo3c9o1_1280.jpg',
-  'http://24.media.tumblr.com/tumblr_lz6q15mm8o1qgjltdo1_1280.jpg',
-  'http://25.media.tumblr.com/tumblr_lqybq53rCI1qjimsvo1_1280.png',
-  'http://25.media.tumblr.com/tumblr_lz7me0Y6JU1qgjltdo1_1280.jpg',
-  'http://24.media.tumblr.com/tumblr_lggvvf2mCm1qgnva2o1_500.gif'
+const carouselItems = [
+  {url: 'http://25.media.tumblr.com/Jjkybd3nScerz2jsjvrqDPix_400.jpg', text: 'wow'},
+  {url: 'http://25.media.tumblr.com/tumblr_lny14qJ7Wu1qdth8zo1_1280.jpg', text: 'what a cat!'},
+  {url: 'http://25.media.tumblr.com/tumblr_mbo4u98wvQ1qhwmnpo1_1280.jpg', text: 'did you see that one?'},
+  {url: 'http://30.media.tumblr.com/tumblr_m26a45YwZA1r6t7tvo1_500.gif', text: 'best cat eveer'},
+  {url: 'http://25.media.tumblr.com/tumblr_m2x23lyfit1qzo3c9o1_1280.jpg', text: 'fo realzies'},
+  {url: 'http://24.media.tumblr.com/tumblr_lz6q15mm8o1qgjltdo1_1280.jpg', text: 'awwwwwwwwwwwwwwwwww'},
+  {url: 'http://25.media.tumblr.com/tumblr_lqybq53rCI1qjimsvo1_1280.png', text: 'awwwhhhhhhhhwwhwhwhwhhhhhhhhh'},
+  {url: 'http://25.media.tumblr.com/tumblr_lz7me0Y6JU1qgjltdo1_1280.jpg', text: 'kitty!'},
+  {url: 'http://24.media.tumblr.com/tumblr_lggvvf2mCm1qgnva2o1_500.gif', text: 'I should check my neko atsume...'}
 ];
 
 function renderImage (url, index, key, change) {
   return (
-    img('.cat', {
+    img('.carouselItem', {
       key,
       props: {src: url, alt: url, height: '500', width: '400'},
       style: {
         opacity: '0',
-        transform: `translateX(${(index + change) * 440}px)`,
-        delayed: {opacity: '1', transform: `translateX(${index * 440}px)`},
-        remove: {opacity: '0', transform: `translateX(${(index - change) * 440}px)`}
+        transition: 'opacity 2s',
+        delayed: {opacity: '1', order: index},
+        remove: {display: 'none', order: 100}
       }
     })
   );
 }
 
-function renderCarousel (catPictures, scrollOffset, change) {
+function renderCarousel (carouselItems, scrollOffset, change) {
   const imagesToShow = 3;
   return (
-    div('.carousel-images', catPictures
+    div('.carousel-images', carouselItems
       .slice(scrollOffset, scrollOffset + imagesToShow)
-      .map((cat, index) => renderImage(cat, index, index + scrollOffset, change))
+      .map((item, index) => renderImage(item.url, index, index + scrollOffset, change))
     )
   );
 }
@@ -59,9 +59,9 @@ export default function Carousel ({DOM}) {
   return {
     DOM: Observable.combineLatest(scroll$, scrollChange$, (scroll, change) =>
       div('.carousel', [
-        button('.previous', {props: {disabled: scroll === 0}}, 'Previous'),
-        button('.next', {props: {disabled: scroll === catPictures.length - 3}}, 'Next'),
-        renderCarousel(catPictures, scroll, change)
+        button('.previous', {props: {'disabled': scroll === 0 }}, 'Previous'),
+        button('.next', {props: {disabled: scroll === carouselItems.length - 3}}, 'Next'),
+        renderCarousel(carouselItems, scroll, change)
       ])
     ),
 
@@ -72,19 +72,7 @@ export default function Carousel ({DOM}) {
 export default function App ({DOM}) {
   const carousel = Carousel({DOM});
 
-  const textForEachCat = [
-    'wow',
-    'what a cat!',
-    'did you see that one?',
-    'best cat eveer',
-    'fo realzies',
-    'awwwwwwwwwwwwwwwwww',
-    'awwwhhhhhhhhwwhwhwhwhhhhhhhhh',
-    'kitty!',
-    'I should check my neko atsume...'
-  ];
-
-  const text$ = carousel.scroll$.map(scroll => textForEachCat[scroll]);
+  const text$ = carousel.scroll$.map(scroll => carouselItems[scroll].text);
 
   return {
     DOM: Observable.combineLatest(text$, carousel.DOM, (text, carouselDOM) =>
